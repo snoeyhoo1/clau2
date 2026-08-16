d7e0b10aa7f916eaf2be731eb48def06b8b9bc9f
snoeyhoo1 <dksgustn1109@gmail.com>
2026-08-16 10:13:17 +0900

    Add files via upload

--- api/signal/[ticker].js
+++ api/signal/[ticker].js
@@ -1,7 +1,10 @@
 // api/signal/[ticker].js
+const { guard } = require('../../lib/auth');
 const { buildSignal } = require('../../lib/signalEngine');
 
 module.exports = async (req, res) => {
+  if (guard(req, res)) return;
+
   try {
     const { ticker, label } = req.query;
     const signal = await buildSignal(ticker, label);
