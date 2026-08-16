8673a377fd742027e7aa7e61347c06f2c05e952f
snoeyhoo1 <dksgustn1109@gmail.com>
2026-08-16 16:48:40 +0900

    Delete api/auth directory

--- api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-// api/auth/logout.js
-
-const { clearSessionCookie } = require('../../lib/auth');
-
-module.exports = async (req, res) => {
-  clearSessionCookie(res);
-  return res.status(200).json({ ok: true });
-};
